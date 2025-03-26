7098d6ed6c62cafdaadcf8520cbe594918b64269
ItSites <front3@itsites.com.ua>
2025-03-26 20:20:49 +0200

    fix swipers navigation

--- src/components/ShopByCategoryItem/ShopByCategoryItem.tsx
+++ src/components/ShopByCategoryItem/ShopByCategoryItem.tsx
@@ -1,16 +1,16 @@
-import { ITag } from "../../types/Types";
-import { Link } from "react-router-dom";
-import './ShopByCategoryItem.scss'
+import { ITag } from '../../types/Types';
+import { Link } from 'react-router-dom';
+import './ShopByCategoryItem.scss';
 
 interface IProps {
-    tag: ITag
+  tag: ITag;
 }
 
 const ShopByCategoryItem = ({ tag }: IProps) => (
-    <Link to='/bruden/shop'>
-        <img src={tag.img} alt={tag.name} width='360' height='290'/>
-        <h3 className="shopByCatItem_title">{tag.name}</h3>
-    </Link>
-)
+  <Link to="/shop">
+    <img src={tag.img} alt={tag.name} width="360" height="290" />
+    <h3 className="shopByCatItem_title">{tag.name}</h3>
+  </Link>
+);
 
 export default ShopByCategoryItem;
