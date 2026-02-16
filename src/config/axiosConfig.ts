6665573e484a6abbff0ba58087a8ac8bb448b027
ItSites <front3@itsites.com.ua>
2026-02-16 11:45:09 +0200

    changed server hosting

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://brudenserver.onrender.com/';
+axios.defaults.baseURL = 'https://brudenserver-production-d02d.up.railway.app/';
+// axios.defaults.baseURL = 'https://brudenserver.onrender.com/';
 // axios.defaults.baseURL = 'http://localhost:3000';
